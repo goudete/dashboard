253543a2414e8a60d7d0ccf20db8656dd3dd03ee
goudete <goudetenrique@gmail.com>
2022-08-12 16:39:47 -0400

    finalizing phantom auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,90 +1,21 @@
 import React, { Suspense } from 'react';
 import { useEffect, useState } from "react";
 import { Link, Outlet, useLocation, useNavigate } from 'react-router-dom';
-import {
-  PublicKey,
-  Transaction,
-} from "@solana/web3.js";
 import mainLogo from './assets/logo.svg';
 import { MainScene } from './components';
+import WalletAuth from './components/walletAuth/walletAuth';
 
 import './App.scss';
 
-type DisplayEncoding = "utf8" | "hex";
-type PhantomEvent = "disconnect" | "connect" | "accountChanged";
-type PhantomRequestMethod =
-  | "connect"
-  | "disconnect"
-  | "signTransaction"
-  | "signAllTransactions"
-  | "signMessage";
-
-interface ConnectOpts {
-  onlyIfTrusted: boolean;
-}
-
-interface PhantomProvider {
-  publicKey: PublicKey | null;
-  isConnected: boolean | null;
-  signTransaction: (transaction: Transaction) => Promise<Transaction>;
-  signAllTransactions: (transactions: Transaction[]) => Promise<Transaction[]>;
-  signMessage: (
-    message: Uint8Array | string,
-    display?: DisplayEncoding
-  ) => Promise<any>;
-  connect: (opts?: Partial<ConnectOpts>) => Promise<{ publicKey: PublicKey }>;
-  disconnect: () => Promise<void>;
-  on: (event: PhantomEvent, handler: (args: any) => void) => void;
-  request: (method: PhantomRequestMethod, params: any) => Promise<unknown>;
-}
-
 
 function App() {
-  const [provider, setProvider] = useState<PhantomProvider | undefined>(undefined);
-  const [walletKey, setWalletKey] = useState<PhantomProvider | undefined>(undefined);
+
   const [mainPage, setMainPage] = useState(true);
-
   const loc = useLocation();
-
-  const getProvider = (): PhantomProvider | undefined => {
-    if ("solana" in window) {
-      // @ts-ignore
-      const provider = window.solana as any;
-      if (provider.isPhantom) {
-        return provider as PhantomProvider;
-      }
-    }
-  };
-
-  const connectWallet = async () => {
-    // @ts-ignore
-    const { solana } = window;
-
-    if (solana) {
-      try {
-        const response = await solana.connect();
-        console.log('wallet account ', response.publicKey.toString());
-        setWalletKey(response.publicKey.toString());
-      } catch (err) {
-        console.log('ERROR connecting', err)
-      }
-    }
-  };
-
-  useEffect(() => {
-    const provider = getProvider();
-
-    if (provider) {
-      setProvider(provider);
-    } else {
-      setProvider(undefined);
-    }
-  }, []);
 
   useEffect(() => {
     setMainPage(loc.pathname === '/')
   }, [loc])
-
 
   return (
     <div className="container">
@@ -101,12 +32,7 @@ function App() {
           </nav>
         </div>
         <div className="container__header-connectWalletButton">
-          {!walletKey && (
-              <button onClick={connectWallet}>
-                Connect to Phantom Wallet
-              </button>
-            )
-          }
+          <WalletAuth />
         </div>
       </div>
       <div className="container__body">
--- src/components/walletAuth/walletAuth.tsx
+++ src/components/walletAuth/walletAuth.tsx
@@ -1,23 +1,111 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import {
+  PublicKey,
+  Transaction,
+} from "@solana/web3.js";
 
-interface Props {
-    walletKey: string | undefined, 
-    connectWallet: () => Promise<{ publicKey: string }>
+import './walletAuth.scss';
+
+
+type DisplayEncoding = "utf8" | "hex";
+type PhantomEvent = "disconnect" | "connect" | "accountChanged";
+type PhantomRequestMethod =
+  | "connect"
+  | "disconnect"
+  | "signTransaction"
+  | "signAllTransactions"
+  | "signMessage";
+
+interface ConnectOpts {
+  onlyIfTrusted: boolean;
 }
 
-function WalletAuth(props: Props) {
-    const { walletKey, connectWallet } = props;
+interface PhantomProvider {
+  publicKey: PublicKey | null;
+  isConnected: boolean | null;
+  signTransaction: (transaction: Transaction) => Promise<Transaction>;
+  signAllTransactions: (transactions: Transaction[]) => Promise<Transaction[]>;
+  signMessage: (
+    message: Uint8Array | string,
+    display?: DisplayEncoding
+  ) => Promise<any>;
+  connect: (opts?: Partial<ConnectOpts>) => Promise<{ publicKey: PublicKey }>;
+  disconnect: () => Promise<void>;
+  on: (event: PhantomEvent, handler: (args: any) => void) => void;
+  request: (method: PhantomRequestMethod, params: any) => Promise<unknown>;
+}
 
-    return (!walletKey ? (
-            <button onClick={connectWallet}>
-                Connect to Phantom Wallet
-            </button>
-        ) : (
-            <>
-                {walletKey}
-            </>
-        )
-    )
+interface WalletKey {
+  walletKey: string,
+  shortWalletKey: string
+}
+
+
+function WalletAuth() {
+  const [provider, setProvider] = useState<PhantomProvider | undefined>(undefined);
+  const [walletKey, setWalletKey] = useState<WalletKey | undefined>(undefined);
+
+
+  const getProvider = (): PhantomProvider | undefined => {
+    if ("solana" in window) {
+      // @ts-ignore
+      const provider = window.solana as any;
+      if (provider.isPhantom) {
+        return provider as PhantomProvider;
+      }
+    }
+  };
+
+  const walletClick = async () => {
+    // @ts-ignore
+    const { solana } = window;
+
+    if (solana) {
+      try {
+        if (!walletKey) {
+          const response = await solana.connect();
+          const keys: WalletKey = {
+            walletKey: response.publicKey.toString(),
+            shortWalletKey: generateShortWalletKey(response.publicKey.toString())
+          }
+
+          setWalletKey(keys);
+        } else {
+          await solana.disconnect();
+          setWalletKey(undefined);
+        }
+      } catch (err) {
+        alert(err)
+      }
+    } else {
+      alert('Please download Phantom');
+    }
+  };
+
+  const generateShortWalletKey = (walletKey: string) => {
+    const hexNotation = '0x';
+    const beginning = walletKey.slice(0, 4);
+    const elipsis = '...';
+    const end = walletKey.slice(40);
+    const shortWalletKey = hexNotation.concat(beginning).concat(elipsis).concat(end);
+
+    return shortWalletKey;
+  };
+
+  useEffect(() => {
+    const provider = getProvider();
+    if (provider) {
+      setProvider(provider);
+    } else {
+      setProvider(undefined);
+    }
+  }, []);
+
+  return <div className="connectWallet" onClick={walletClick}>
+    {walletKey && <span className="connectWallet-text">{walletKey.shortWalletKey}</span>}
+    {!walletKey && <span className="connectWallet-text">connect wallet</span>}
+  </div>;
 }
 
 export default WalletAuth;
