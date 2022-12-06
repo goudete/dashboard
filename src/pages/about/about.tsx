1ca1a53a6d79daf1cd2dabc4207057a9f89b41f5
goudete <goudetenrique@gmail.com>
2022-12-05 19:23:38 -0500

    smoothing text

--- src/pages/about/about.tsx
+++ src/pages/about/about.tsx
@@ -10,15 +10,13 @@ function About() {
                     </h1>
                     <p className={"animate__animated animate__fadeInUp"}>
                         SendVaro exists to help you send and receive money between your
-                        family and friends regardless of what country you live in
-                        or how much you want to send.
+                        family and friends regardless of where you live.
                     </p>
                     <p className={"animate__animated animate__fadeInUp"}>
                         Current products are time-consuming, frustrating and expensive
-                        because they are built on top of fragmented technology that is
-                        different in every country. SendVaro is not the current
-                        competitors' with a shiny paint job, it is the next generation
-                        of digital payments.
+                        because they are built on top of fragmented international payment 
+                        rails. SendVaro is not the current rails with a shiny new paint 
+                        job, it is the next generation of digital payments.
                     </p>
                     <p className={"animate__animated animate__fadeInUp"}>
                         When I moved from Mexico City to Los Angeles for university
@@ -28,7 +26,7 @@ function About() {
                     <p className={"animate__animated animate__fadeInUp"}>
                         High transaction fees, lack of transparency, and time delays
                         meant a guaranteed headache anytime I had to deal with cross
-                        boarder payments between family and friends.
+                        boarder payments between my family and friends in Mexico.
                     </p>
                     <p className={"animate__animated animate__fadeInUp"}>
                         SendVaro is the friends and family borderless payment
